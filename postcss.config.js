00432f94d51f0e3dd484b5f636fe223f0c68e2e1
garrettbland <gmorganbland@gmail.com>
2019-06-07 12:36:38 -0500

    added firebase

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.VUE_APP_PURGECSS === 'true' ? [
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./src/**/*.vue', './public/**/*.html'],
+        extractors: [
+          {
+            extractor: class {
+              static extract(content) {
+                console.log('purging css...');
+                return content.match(/[a-zA-Z0-9-:_/]+/g) || [];
+              }
+            },
+            extensions: ['vue', 'html'],
+          },
+        ],
+      })
+    ] : [],
+  ]
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <div class="flex h-auto sm:h-auto md:h-screen items-center justify-center">
+    	<div class="container max-w-4xl bg-white flex flex-wrap md:rounded-lg md:shadow-lg">
+    		<div class="w-full sm:w-full md:w-1/2">
+    			<img src="https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="w-full md:rounded-l-lg"/>
+    		</div>
+    		<div class="flex flex-col justify-between w-full sm:w-full md:w-1/2">
+    			<div class="p-4">
+	    			<h1 class="text-4xl font-bold font-sans">
+	    				Taylors Cat
+	    			</h1>
+	    			<p class="text-lg font-sans leading-regular text-gray-800">
+	    				Welcome to Taylors Cat. Taylor uploads a new picture of his cat <strong>Millie</strong> on a daily basis. Check back everyday for a new cute picture! 
+	    			</p>
+	    		</div>
+	    		<div class="md:rounded-br-lg">
+	    			<!-- <span class="m-4 bg-indigo-700 px-3 py-1 text-xs text-indigo-100 rounded-full">New</span> -->
+	    			<div class="px-4 py-2 text-sm text-gray-600 italic">
+	    				Want to be reminded to check Taylors new cat pictures? Sign up below to receive nothing but a daily reminder.
+	    			</div>
+	    			<div class="bg-gray-100 flex justify-between md:rounded-br-lg">
+	    				<input v-model="email" name="signup" placeholder="you@email.com" type="email" class="text-gray-900 border-2 border-transparent bg-transparent focus:outline-none p-4 w-4/5"/>
+	    				<button class="flex text-gray-900 p-4 focus:outline-none md:rounded-br-lg" v-bind:class="emailStatus" @click="subscribe()" @prevent.default>
+	    					<span class="h-4 w-4 mr-4 pt-px">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="261"><path d="M20 6L9 17 4 12"/></svg>
+	    					</span>
+	    					Subscribe
+	    				</button>
+	    			</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data(){
+  	return {
+  		email:''
+  	}
+  },
+  methods:{
+  	validEmail(email){
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    subscribe(){
+    	if(this.validEmail(this.email)){
+    		window.alert('This feature isnt working yet')
+    	}
+    }
+  },
+  computed:{
+  	emailStatus(){
+  		if(this.validEmail(this.email)){
+  			return 'text-green-500 opactiy-100 hover:text-green-700'
+  		}else{
+  			return 'opacity-50 cursor-not-allowed'
+  		}
+  	}
   }
 }
 </script>
